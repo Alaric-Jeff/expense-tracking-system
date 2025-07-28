e44a57d54411f93707588bebba6a6a3ee40ef913
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-07-28 11:33:35 +0800

    fixed drizzle config path

--- fastify-backend/drizzle.config.js
+++ fastify-backend/drizzle.config.js
@@ -4,8 +4,8 @@ import {defineConfig} from 'drizzle-kit'
 dotenv.config();
 
 export default defineConfig({
-    out: '../../drizzle',
-    schema: './models/userModel.ts',
+    out: './drizzle',
+    schema: './src/models/*.ts',
     dialect: 'postgresql',
     dbCredentials: {
         url: String(process.env.DATABASE_URL)
