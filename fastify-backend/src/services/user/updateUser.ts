e74ea469a206dd445e3918c74e14fe12f79e0c13
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-07-29 15:17:44 +0800

    dynamic service for updates

--- fastify-backend/src/services/user/updateUser.ts
+++ fastify-backend/src/services/user/updateUser.ts
@@ -1,16 +1,36 @@
 import { FastifyInstance } from "fastify";
+import { UpdateUserBodyType } from "../../schemas/user/updateBodySchema.js";
+import users_table from "../../models/userModel.js";
+import { eq } from "drizzle-orm";
 
-async function UpdateUserService(fastify: FastifyInstance){
-    try {
-        
-    } catch (err: unknown) {
-      if(err instanceof Error){
-        fastify.log.error(`Error occured in update service, err: ${err.message}`)
-      } else{
-        fastify.log.error(`Unknown error occured in update service, err: ${err}`)
-      } 
-      throw new Error(`UpdateUserService() failed`)
+async function UpdateUserService(fastify: FastifyInstance, body: UpdateUserBodyType) {
+  try {
+    const { user_id, ...rest } = body;
+    const updates: Record<string, any> = {};
+    for (const [key, value] of Object.entries(rest)) {
+      if (value !== undefined) {
+        updates[key] = value;
+      }
     }
-};
+
+    if (Object.keys(updates).length === 0) {
+      fastify.log.warn("No update fields provided.");
+      return;
+    }
+
+    await fastify.db
+      .update(users_table)
+      .set(updates)
+      .where(eq(users_table.user_id, user_id));
+
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      fastify.log.error(`Error occurred in update service: ${err.message}`);
+    } else {
+      fastify.log.error(`Unknown error occurred in update service.`);
+    }
+    throw new Error(`UpdateUserService() failed`);
+  }
+}
 
 export default UpdateUserService;
